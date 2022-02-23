7c7bb2075750d9306f93f2becc045998d100976d
Houda Bouamor <ladyhodhod@gmail.com>
2022-02-23 11:37:28 +0300

    added the index action in the Pets Controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,9 @@
 class PetsController < ApplicationController
+
+    def index
+        # get data on all pets and paginate the output to 10 per page
+        @active_pets = Pet.active.alphabetical.paginate(page: params[:page]).per_page(10)
+        @inactive_pets = Pet.inactive.alphabetical.to_a
+    end
+
 end
